3ffe028623c7dc0dbdd257889041b6efdb8f4818
Roman PARYGIN <rompik@mail.ru>
2023-12-15 15:27:37 +0100

    FireStation example

---
+++ FireStation/FireStation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireStation
+{
+    public class FireStation
+    {
+        public Area area;
+
+        public void OnFire(Area address)
+        {
+            if (address == area)
+            {
+                Console.WriteLine("Go " + address);
+            }
+            else
+            {
+                Console.WriteLine("Sorry");
+            }
+
+        }
+    }
+}
---
+++ FireStation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FireStation
+{
+    public enum Area 
+    { 
+        Center, Suburban
+    }
+
+    public delegate void FireHandler(Area address);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var city = new City();
+            
+            city.Add(new Building( Area.Center ));
+
+            city.StartFire();
+        }
+    }
+}
